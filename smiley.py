8bc0ab3f3314c9a130343442d7e53cf287b3dbfa
geckro <94733731+geckro@users.noreply.github.com>
2023-10-25 17:37:25 +0800

    addressed requirements of portfolio

--- smiley.py
+++ smiley.py
@@ -7,12 +7,16 @@ class Smiley:
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
     BLANK = (0, 0, 0)
+    
+    # because for some reason this didnt exist yet. might be intentional
+    BLUE = (0, 0, 255)
 
-    def __init__(self):
+    def __init__(self, complexion=YELLOW):
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
+        self.my_complexion = complexion
 
-        Y = self.YELLOW
+        Y = complexion
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -37,3 +41,6 @@ class Smiley:
         Show the smiley on the screen.
         """
         self.sense_hat.set_pixels(self.pixels)
+
+    def complexion(self):
+        return self.my_complexion
